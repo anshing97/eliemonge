adcd9d30cab0f82f97162ac616ad9c725dc11d2b
Ben Chao <bchao@cs.washington.edu>
2013-07-26 18:14:18 -0400

    Adding converts

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'nokogiri'
 gem 'mechanize'
 gem 'paperclip'
 gem 'aws-sdk'
+gem "rmagick", require: 'RMagick'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,12 @@
 class Image < ActiveRecord::Base
   # attr_accessible :title, :body
   attr_accessible :image
-  has_attached_file :image
-
+  has_attached_file :image, 
+    :styles => {
+    :large => "900x600>",
+    :medium => "600x400>",
+    :square => "100x100#", 
+    :small => "300x200>" 
+  },
+  :convert_options => { :all => '-auto-orient' }
 end
