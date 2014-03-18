c1d1d37f7d20b59b7e966d92903078aa851e5cf4
Ben Chao <bchao@cs.washington.edu>
2014-03-18 10:59:59 -0400

    Removing ? from image url

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -35,7 +35,7 @@ class SearchController < ApplicationController
           @image = Image.new(:image => File.open(file_name))
 
           if @image.save
-            el['thumb'] = @image.image.url 
+            el['thumb'] = @image.image.url(:original).split('?').first
           end 
         end 
 
